dc9d71ae286f393cf0f4f89456e588786bf2d1a0
Rodrigo Falcao <rodrigofal44@gmail.com>
2025-06-15 17:09:25 +0100

    ex00 done.

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,25 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 16:44:00 by rde-fari          #+#    #+#             */
-/*   Updated: 2025/06/15 16:44:01 by rde-fari         ###   ########.fr       */
+/*   Updated: 2025/06/15 17:08:11 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <cctype>
+
+int main (int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			for (int j = 0; av[i][j]; j++)
+				std::cout << char(toupper(av[i][j]));
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
